b4114a54393b8d449f62dce83f5dd85ec523b7cf
wolofare <wolofare1@gmail.com>
2023-08-20 17:53:59 +0300

    setup: React project with Vite

---
+++ 8-react-typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import Container from "./components/Container";
+
+export default function App() {
+  return (
+    <div>
+      <Container>
+        <h1>Home</h1>
+      </Container>
+    </div>
+  );
+}
---
+++ 8-react-typescript/src/components/Container.tsx
@@ -0,0 +1,7 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return <div className="container mx-auto px-4 sm:px-6">{children}</div>;
+}
