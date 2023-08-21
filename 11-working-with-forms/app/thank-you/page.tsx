85d9c4e87dd9f255a35055ed4bfb651c3bac8545
wolofare <wolofare1@gmail.com>
2023-08-21 11:15:13 +0300

    feature: Thank You page

---
+++ 11-working-with-forms/app/thank-you/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+import React from "react";
+
+export default function ThankYou() {
+  return (
+    <Container>
+      <section className="py-10 max-w-2xl mx-auto">
+        <p className="bg-green-50 text-green-500 p-6">
+          Thank You for submitting your details
+        </p>
+      </section>
+    </Container>
+  );
+}
