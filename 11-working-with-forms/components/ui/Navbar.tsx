69e62776efe0aeac7fcdaea46f987fb3c57946c5
wolofare <wolofare1@gmail.com>
2023-08-22 09:01:37 +0300

    zod validations

--- 11-working-with-forms/components/ui/Navbar.tsx
+++ 11-working-with-forms/components/ui/Navbar.tsx
@@ -10,13 +10,19 @@ export default function Navbar() {
           <h1 className="text-xl font-semibold">
             <Link href={"/"}>Form Validation</Link>
           </h1>
-          <nav>
-            <li className="list-none m-0 p-0">
+          <nav className="">
+            <li className="list-none m-0 p-0 flex gap-5 justify-between items-center">
               <Link
                 href="/contact"
                 className="text-gray-600 hover:text-green-500 transition-all duration-300 ease-in-out"
               >
                 Contact
+              </Link>
+              <Link
+                href="/login"
+                className="text-gray-600 hover:text-green-500 transition-all duration-300 ease-in-out"
+              >
+                Login
               </Link>
             </li>
           </nav>
